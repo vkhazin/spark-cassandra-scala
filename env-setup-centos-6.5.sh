fb5fef5737a7682261bc991f8d45c50720c46434
vladimir.khazin <vladimir.khazin@icssolutions.ca>
2017-10-05 14:12:19 -0400

    Latest spark

--- env-setup-centos-6.5.sh
+++ env-setup-centos-6.5.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Removing previous version of java
-sudo yum remove java-1.7.0-openjdk-devel.x86_64 -y
-sudo yum remove java-1.7.0-openjdk.x86_64 -y
+# sudo yum remove java-1.7.0-openjdk-devel.x86_64 -y
+# sudo yum remove java-1.7.0-openjdk.x86_64 -y
 # Download Install Java 8
-curl -O -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm
-sudo rpm -ivh ./jdk-8u144-linux-x64.rpm
-rm ./jdk-8u144-linux-x64.rpm
+# curl -O -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm
+# sudo rpm -ivh ./jdk-8u144-linux-x64.rpm
+# rm ./jdk-8u144-linux-x64.rpm
 
 # Installing SBT *
-curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
-sudo yum install sbt -y
+# curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+# sudo yum install sbt -y
 
 # Download and install Spark
-curl -O https://d3kbcqa49mib13.cloudfront.net/spark-1.4.1-bin-hadoop2.6.tgz
-tar xvf spark-1.4.1-bin-hadoop2.6.tgz
-rm ./spark-1.4.1-bin-hadoop2.6.tgz
+curl -O https://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz
+tar xvf spark-2.2.0-bin-hadoop2.7.tgz
 sudo mkdir /usr/local/spark
-sudo mv ./spark-1.4.1-bin-hadoop2.6/* /usr/local/spark
-rmdir ./spark-1.4.1-bin-hadoop2.6
+sudo mv ./spark-2.2.0-bin-hadoop2.7/* /usr/local/spark
+rmdir ./spark-2.2.0-bin-hadoop2.7
+rm ./spark-2.2.0-bin-hadoop2.7.tgz
 echo "PATH=$PATH:$HOME/bin:/usr/local/spark/bin" >> ~/.bashrc
 echo "SPARK_HOME="/usr/local/spark/ >> ~/.bashrc
 source ~/.bashrc
@@ -28,5 +28,5 @@ source ~/.bashrc
 # sudo yum install scala-2.11.8.rpm
 
 # Build Project
-sbt assembly
+# sbt assembly
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,15 @@
-import com.datastax.spark.connector._
+// import com.datastax.spark.connector._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object Main extends App {
-//   println("Hello, World!")
+  println("Hello, World!")
   
-		val host = "13.59.53.178"
-		val conf = new SparkConf(true).set("spark.cassandra.connection.host", host)
-		val sc = new SparkContext(conf)    
-    val digitalAsset = sc.cassandraTable("perfda", "digitalasset_3_0_2")
-    println(digitalAsset.first)
+// 		val host = "13.59.53.178"
+// 		val conf = new SparkConf(true).set("spark.cassandra.connection.host", host)
+// 		val sc = new SparkContext(conf)    
+//     val digitalAsset = sc.cassandraTable("perfda", "digitalasset_3_0_2")
+//     println(digitalAsset.first)
 }
 
