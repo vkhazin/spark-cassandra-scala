fa0bbed4246490acac0130cae75b597bf828e5dd
Ubuntu <ubuntu@ip-172-31-23-144.us-east-2.compute.internal>
2017-10-10 01:25:59 +0000

    reading count from cassandra

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ cd ./setup
 chmod +x *.sh
 ```
 ## Ubuntu: ```./ubuntu-16.04.sh```
+
+# Submitting Job to Spark
+chmod +x ./submit.sh
+./submit.sh
+*Please Note:* sbt corrupts the echo in terminal, use ```reset``` to get the echo back
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,11 +4,20 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object Main extends App {
-    println("Hello, World!")
+    println("Connecting to cassandra")
+  
 		val host = "172.31.36.205"
+    val keyspace = "perf8da"
+    val table = "digitalasset_3_0_2"
+  
 		val conf = new SparkConf(true).set("spark.cassandra.connection.host", host)
-		val sc = new SparkContext(conf)    
-    val digitalAsset = sc.cassandraTable("perf8da", "digitalasset_3_0_2")
-    println(digitalAsset.count)
+		val sc = new SparkContext(conf)
+	
+    val digitalAsset = sc.cassandraTable(keyspace, table).limit(10)
+
+		print("Records received: ")
+    println(digitalAsset.cassandraCount())
+	
+    println("Finished processing")
 }
 
