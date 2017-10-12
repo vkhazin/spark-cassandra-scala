8506d95661ae693d478a47d20b151c816e023d3e
vkhaizn <vladimir.khazin@icssolutions.ca>
2017-10-12 03:57:39 +0000

    Working on windows 2016 setup

---
+++ setup/windows-2016.ps1
@@ -0,0 +1,8 @@
+choco install jdk8 -y
+choco install sbt -y
+
+# Spark
+choco install 7zip.portable -y
+curl https://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz > spark-2.2.0-bin-hadoop2.7.tgz
+
+
