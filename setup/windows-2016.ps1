e60818144f4106bb60ff6b46db707a5d515e6ae3
vkhaizn <vladimir.khazin@icssolutions.ca>
2017-10-13 10:47:53 -0400

    Adding winutils

--- setup/windows-2016.ps1
+++ setup/windows-2016.ps1
@@ -4,14 +4,39 @@ choco install sbt -y
 # Spark
 choco install 7zip.portable -y
 $sparkVersion="spark-2.2.0-bin-hadoop2.7"
+$sparkFolder="C:\spark\"
 $uri="https://d3kbcqa49mib13.cloudfront.net/$sparkVersion.tgz"
 $path=(Convert-Path .)
 $filename=$path + "\"+ $sparkVersion + ".tgz"
 (New-Object System.Net.WebClient).DownloadFile($uri, $filename)
  7z x .\$sparkVersion.tgz
  7z x .\$sparkVersion.tar
-mkdir 'C:\spark'
- mv .\spark-2.2.0-bin-hadoop2.7\* 'C:\spark\'
-$env:Path += ";C:\spark\bin"
+mkdir $sparkFolder
+ mv .\spark-2.2.0-bin-hadoop2.7\* $sparkFolder
 rm .\$sparkVersion.tar
 rm .\$sparkVersion.tgz
+set SPARK_HOME=$sparkFolder
+
+# Winutils
+$uri="https://github.com/steveloughran/winutils/raw/master/hadoop-2.7.1/bin/winutils.exe"
+$filename=$path + "\"+ "winutils.exe"
+(New-Object System.Net.WebClient).DownloadFile($uri, $filename)
+mv ./winutils.exe "$sparkFolder/bin"
+set HADOOP_HOME=$sparkFolder
+
+# Set path
+$oldpath = (Get-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH).path
+$newpath = "$oldpath;$sparkFolder\bin"
+Set-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH -Value $newPath
+
+
+
+# testing...
+# $path=(Convert-Path .)
+# $sparkFolder="C:\spark\"
+# set SPARK_HOME=$sparkFolder
+# $uri="https://github.com/steveloughran/winutils/raw/master/hadoop-2.7.1/bin/winutils.exe"
+# $filename=$path + "\"+ "winutils.exe"
+# (New-Object System.Net.WebClient).DownloadFile($uri, $filename)
+# mv ./winutils.exe "$sparkFolder/bin"
+# set HADOOP_HOME=$sparkFolder
