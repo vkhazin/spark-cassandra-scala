f4c08f4c868fac8fafb04e26c0780d537f6aff63
Ubuntu <ubuntu@ip-172-31-13-14.us-east-2.compute.internal>
2017-11-14 00:04:44 +0000

    Updating setup and submit scripts

--- setup/ubuntu-16.04.sh
+++ setup/ubuntu-16.04.sh
@@ -8,6 +8,7 @@ echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.li
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt-get update
 sudo apt-get install sbt
+sudo apt-get install bc -y
 
 # Install Spark
 ./spark.sh
